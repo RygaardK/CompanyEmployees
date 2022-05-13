c17aba4702bef282891ad1e0609b6dea49ead619
Kristoffer Rygaard <rygaardkristoffer@gmail.com>
2022-05-13 18:38:19 +0200

    first push, added logger service and interface - page 42 of 393

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Contracts; // Didnt tell us to add, Contract and logger should be globaly added.
+using LoggerService;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        // .WithOrigins("https://example.com")
+        // .WithMethods("POST", "GET")
+        // .WithHeaders("accept", "contenttype")
+        public static void ConfigureCors(this IServiceCollection services) =>
+         services.AddCors(options =>
+         {
+             options.AddPolicy("CorsPolicy", builder =>
+             builder.AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+         });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+                {
+                    //options.AutomaticAuthentication = true;
+                    //options.AuthenticationDisplayName = "Krille";
+                    //options.ForwardClientCertificate = true;
+                });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    }
+
+}
---
+++ ReadMe.txt
@@ -0,0 +1,17 @@
+﻿This is a test project for learning API, Routing, Authentication and so on...
+
+
+
+
+
+
+
+***********************************************
+.\internal_logs\internallog.txt
+You can find the internal logs at the project root, and the logs folder in
+the bin\debug folder of the main project once we start the app. Once the
+application is published both folders will be created at the root of the
+output folder which is what we want.
+***********************************************
+
+
